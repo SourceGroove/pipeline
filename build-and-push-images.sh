68db60b641813691405001847ad601b4c9412912
Seth Jordan <sjordan@madenasolutions.com>
2019-10-14 13:58:57 -0400

    laravel

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -1,7 +1,15 @@
 echo "Building images..."
-docker build -t images/sourcegroove/maven-3.6-jdk-8-onbuild maven-3.6-jdk-8
-docker build -t images/sourcegroove/springboot-jdk-8-onbuild springboot-jdk-8
+docker build -t sourcegroove/springboot-jdk-8-build images/springboot-jdk-8-build
+docker build -t sourcegroove/springboot-jdk-8 images/springboot-jdk-8
+docker build -t sourcegroove/angular-8-build images/angular-8-build
+docker build -t sourcegroove/angular-8 images/angular-8
+docker build -t sourcegroove/laravel-php-7-build images/laravel-php-7-build
+docker build -t sourcegroove/laravel-php-7 images/laravel-php-7
 
 echo "Pusing images..."
-docker push sourcegroove/maven-3.6-jdk-8-onbuild
-docker push sourcegroove/springboot-jdk-8-onbuild
+docker push sourcegroove/springboot-jdk-8-build
+docker push sourcegroove/springboot-jdk-8
+docker push sourcegroove/angular-8-build
+docker push sourcegroove/angular-8
+docker push sourcegroove/laravel-php-7-build
+docker push sourcegroove/laravel-php-7

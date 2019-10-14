1c62c7a23ed8d462c7721a871e3a16a6a350c704
Seth Jordan <sjordan@madenasolutions.com>
2019-10-14 14:19:07 -0400

    Setup

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 echo "Building images..."
 docker build -t sourcegroove/springboot-jdk-8-build images/springboot-jdk-8-build
-docker build -t sourcegroove/springboot-jdk-8 images/springboot-jdk-8
+docker build -t sourcegroove/springboot-jdk-8-onbuild images/springboot-jdk-8
 docker build -t sourcegroove/angular-8-build images/angular-8-build
-docker build -t sourcegroove/angular-8 images/angular-8
-docker build -t sourcegroove/laravel-php-7-build images/laravel-php-7-build
-docker build -t sourcegroove/laravel-php-7 images/laravel-php-7
+docker build -t sourcegroove/angular-8-onbuild images/angular-8
+docker build -t sourcegroove/laravel-php-7-onbuild images/laravel-php-7
 
 echo "Pusing images..."
 docker push sourcegroove/springboot-jdk-8-build
-docker push sourcegroove/springboot-jdk-8
+docker push sourcegroove/springboot-jdk-8-onbuild
 docker push sourcegroove/angular-8-build
-docker push sourcegroove/angular-8
-docker push sourcegroove/laravel-php-7-build
+docker push sourcegroove/angular-8-onbuild
 docker push sourcegroove/laravel-php-7
